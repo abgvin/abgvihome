21520b8994124086a1a3a578bafdf92fdc6cd60c
abgvin <muhamadapri10@gmail.com>
2020-10-27 20:06:07 +0700

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const sideNavElement = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sideNavElement, {
+    'edge': 'right',
+  });
+});
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
